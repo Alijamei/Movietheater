6b6a683f094c3cbd65841e772d3807baf916361f
bob <bob@>
2022-01-19 18:39:45 +0300

    master

--- frontend/src/compnents/footer.jsx
+++ frontend/src/compnents/footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer(){
   return(
                 <div class="greyfooter">
-                    <ul className="ulil"> 
+                  <ul className="ulil"> 
                     <li className="lil">About</li>
                     <li className="lil">PRIVACY POLICY</li>
                     <li className="lil">ALL right reserved 2021</li>
