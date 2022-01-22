4d9807ff46c33a2a6eba779ac300c673f136b684
bob <bob@>
2022-01-22 16:51:26 +0300

    master

--- backend/app.js
+++ backend/app.js
@@ -65,13 +65,13 @@ app.use('/', postroutes);
 // if (process.env.NODE_ENV === 'production') {
 
 
-  // app.use(express.static(path.join(__dirname,'..', 'frontend', 'build')));
+  app.use(express.static(path.join(__dirname,'..', 'frontend', 'build')));
 
-  // app.get('*', (req, res) =>
-  //   res.sendFile(
-  //     path.resolve(__dirname, '..', 'frontend', 'build', 'index.html')
-  //   )
-  // );
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '..', 'frontend', 'build', 'index.html')
+    )
+  );
 
 
 
