940b1547514c773f4c621bc534d7dde145441e22
bob <bob@>
2022-01-22 17:22:19 +0300

    master

--- backend/app.js
+++ backend/app.js
@@ -67,11 +67,11 @@ app.use('/', postroutes);
 
   app.use(express.static(path.join(__dirname,'..', 'frontend', 'build')));
 
-  // app.get('*', (req, res) =>
-  //   res.sendFile(
-  //     path.resolve(__dirname, '..', 'frontend', 'build', 'index.html')
-  //   )
-  // );
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '..', 'frontend', 'build', 'index.html')
+    )
+  );
 
 
 
