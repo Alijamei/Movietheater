13dc4c032acca362dc7ec4bc0ac327761f7891b3
Alijamei <66800608+Alijamei@users.noreply.github.com>
2022-07-20 00:23:58 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -64,17 +64,14 @@ app.use('/', cartroutes);
 app.use('/', postroutes);  
 
 
-app.use(express.static(path.join(__dirname,'..', 'frontend', 'build')));
- if (process.env.NODE_ENV === 'production') {
-
- app.use('*', (req, res) =>
-     res.sendFile(
-         path.resolve(__dirname, '..', 'frontend', 'build', 'index.html')
-     )
-   );
-
+app.use(express.static(path.join(__dirname, '../frontend/build')))
+app.get('*', (req, res) => { 
+  res.sendFile(path.resolve(__dirname,'../frontend/build/index.html'), err => {
+     if (err) { res.status(500).send(err)  }
+  })
+})
 // if (process.env.NODE_ENV === 'production') {
- //    app.use(express.static(path.join(__dirname, '../frontend/build')));
+//     app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 //  app.get('*', (req, res) =>
 //   res.sendFile(
