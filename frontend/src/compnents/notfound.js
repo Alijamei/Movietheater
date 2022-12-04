32783a2867ed97e1b336dc5d8e641bb7124721dd
Alijamei <66800608+Alijamei@users.noreply.github.com>
2022-12-04 22:33:57 +0300

    Update notfound.js

--- frontend/src/compnents/notfound.js
+++ frontend/src/compnents/notfound.js
@@ -1,16 +1,11 @@
 import React from "react";
 
-
 function Notfound(){
            
-       return(
-                
+       return(                
             <div style={{backgroundColor: '#F0FFFF',position:'relative',display:"inline-block",width:'100%',height:'700px'}}>
-                   <h1>Not found </h1>
-                   
-              </div>
-                
-
+                   <h1>Not found </h1>                   
+              </div>               
        )
 }
 export default Notfound;
