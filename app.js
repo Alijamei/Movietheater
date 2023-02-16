33b5b6354a4a5a620627db56ce314e52e076fff2
Alijamei <66800608+Alijamei@users.noreply.github.com>
2023-02-16 16:01:40 +0300

    production

--- app.js
+++ app.js
@@ -63,17 +63,17 @@ app.use('/', postRoutes);
 
 app.use(express.static(path.join(__dirname, './Client/build')))
 
-app.get('*', (req, res) => { 
-  res.sendFile(path.resolve(__dirname,'./Client/build/index.html'), err => {
-     if (err) { res.status(500).send(err)  }
-  })
-})
-// if (process.env.NODE_ENV === 'production') {
-//   //*Set static folder up in production
-//   app.use(express.static('Client/build'));
+// app.get('*', (req, res) => { 
+//   res.sendFile(path.resolve(__dirname,'./Client/build/index.html'), err => {
+//      if (err) { res.status(500).send(err)  }
+//   })
+// })
+if (process.env.NODE_ENV === 'production') {
+  //*Set static folder up in production
+  app.use(express.static('Client/build'));
 
-//   app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'Client', 'build','index.html')));
-// }
+  app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'Client', 'build','index.html')));
+}
  app.listen(PORT,function(){
       console.log(`Server is running on port ${PORT}`)
     
